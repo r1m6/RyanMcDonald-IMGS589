d126079f61031b994eac2fe1efe1f12448d6e3a6
Ryan McDonald <rbm8433@rit.edu>
2025-01-23 12:59:44 -0500

    Add files via upload

---
+++ Homework1.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+## Problem 1
+
+# Loading data
+data = np.load('sentinel2_rochester.npy')
+print(data.shape)
+
+# Labels for each band
+band_labels = [
+    "1 - Coastal Aerosol (443nm)",
+    "2 - Blue (490nm)",
+    "3 - Green (560nm)",
+    "4 - Red (665nm)",
+    "5 - Red Edge 1 (705nm)",
+    "6 - Red Edge 2 (740nm)",
+    "7 - Red Edge 3 (783nm)",
+    "8 - NIR (842nm)",
+    "8A - Narrow NIR (865nm)",
+    "9 - Water Vapor (945nm)",
+    "11 - SWIR 1 (1610nm)",
+    "12 - SWIR 2 (2190nm)"
+]
+
+# Creating image montage
+rows, cols = 3, 4
+fig, axes = plt.subplots(rows, cols, figsize=(15, 10))
+
+# Feeding images to montage
+for i in range(rows * cols):
+    ax = axes[i // cols, i % cols]
+    ax.imshow(data[:, :, i], cmap='gray')
+    ax.set_title(f'Band {i+1}')
+    ax.set_title(band_labels[i])
+    ax.axis('off')
+
+# Displaying montage
+plt.tight_layout()
+plt.show()
+
+## Problem 2
+
+
+
+
+
+print('done')
